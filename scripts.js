7d67ff3e47f879bce9c52dbc213330b5f778e2f4
Pedro Martin <pataruco@gmail.com>
2018-03-09 12:33:16 +0000

    Set a button spinner draft

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const svg = document.getElementById('js-svg');
 const button = document.getElementById('js-button');
 
-const svgCheckmark = document.getElementById('js-svg-checkmark');
+const svgCheckmark = document.getElementById('js-svg-alltogether');
 const buttonCheckmark = document.getElementById('js-button-checkmark');
 
 
@@ -17,3 +17,29 @@ const showSuccessCheckmark = (event) => {
 
 button.addEventListener('click', showSuccess);
 buttonCheckmark.addEventListener('click', showSuccessCheckmark);
+
+/*Transition listeners*/
+
+const circle = document.getElementById('js-svg-circle');
+const checkmark = document.getElementById('js-svg-checkmark');
+ 
+circle.addEventListener('transitionend', (event) =>{
+  console.log('circle transitionend', event);
+});
+
+checkmark.addEventListener('transitionend', (event) =>{
+  console.log('checkmark transitionend', event);
+});
+
+
+
+/*Button Spinner*/
+
+const triggerButton = document.getElementById('js-button-buttonspinner');
+const buttonSpinnerSvg = document.getElementById('js-svg-buttonspinner');
+
+
+triggerButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  buttonSpinnerSvg.classList.add('success');
+})
