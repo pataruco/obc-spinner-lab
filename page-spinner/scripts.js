eb05da2080e614709a3ad27245c4e55c35e2d320
Pedro Martin <pataruco@gmail.com>
2018-03-13 11:38:38 +0000

    Finish loading page

--- page-spinner/scripts.js
+++ page-spinner/scripts.js
@@ -1,9 +1,15 @@
 const loadingWrapper = document.getElementById('js-loading-wrapper');
-const button = document.getElementById('js-button');
+const successButton = document.getElementById('js-success-button');
+const loadingButton = document.getElementById('js-loading-button');
 const checkmark = document.getElementById('js-checkmark');
 const animationMessage = document.getElementById('js-animation-message');
 
-button.addEventListener('click', (event)=>{
+loadingButton.addEventListener('click', (event)=>{
+  event.preventDefault();
+  loadingWrapper.classList.add('loading');
+});
+
+successButton.addEventListener('click', (event)=>{
   event.preventDefault();
   loadingWrapper.classList.add('success')
 });
